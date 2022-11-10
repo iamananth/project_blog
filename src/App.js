4a4f831a8b06d986c7b6d604024ce17892c00c21
iamananth <ananthrshenoy@gmail.com>
2022-11-11 00:06:21 +0530

    pages created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticleListpage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
