f25acd2e6c4db295596a665446a226fd833558b8
iamananth <ananthrshenoy@gmail.com>
2022-11-13 21:50:45 +0530

    created article content and linked it

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,6 +1,18 @@
+import { Link } from 'react-router-dom';
+import articles from './article-content';
+
 const ArticleListPage = () => {
     return (
-        <h1>This is the article list page!</h1>
+        <>
+            <h1>Articles</h1>
+            {articles.map(article => (
+                <Link key={article.name} className='article-list-item' to={`/articles/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}.....</p>
+                </Link>
+            ))}
+        </>
+
     )
 }
 
